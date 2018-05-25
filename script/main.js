4055ef94abe6eb357ed7a4db7b57b1d64dc5fa41
mahmudulshuvo <shuvo0201@gmail.com>
2018-05-25 16:47:54 +0600

    initial commit

---
+++ script/main.js
@@ -0,0 +1,22 @@
+
+
+var myAppModule = angular.module('myApp', [])
+myAppModule.controller('Ctrler', function($scope) {
+
+   $scope.copyText = function() {
+       if ($scope.sa == null) {
+           document.getElementById("demo").innerHTML = "Please input text!"
+       }
+       else {
+           document.getElementById("demo").innerHTML = "Copy Text"
+           $scope.ba = angular.copy($scope.sa);
+       }
+
+   };
+
+   $scope.clearText = function() {
+       document.getElementById("demo").innerHTML = "Clear Text"
+       $scope.sa = null;
+       $scope.ba = null;
+   }
+});
