430a53c6c2581a12a2b823fb0f5bd3db2bb8133b
mahmudulshuvo <shuvo0201@gmail.com>
2018-06-01 12:23:28 +0600

    new changes

--- script/main.js
+++ script/main.js
@@ -5,17 +5,20 @@ myAppModule.controller('Ctrler', function($scope) {
 
    $scope.copyText = function() {
        if (!$scope.sa) {
-           document.getElementById("demo").innerHTML = "Please input text!"
+           document.getElementById("left").innerHTML = "Please input text!"
+           document.getElementById("right").innerHTML = "Please input text!"
        }
        else {
-           document.getElementById("demo").innerHTML = "Copy Text"
+           document.getElementById("left").innerHTML = "Copy Text"
+           document.getElementById("right").innerHTML = "Copy Text"
            $scope.ba = angular.copy($scope.sa);
        }
 
    };
 
    $scope.clearText = function() {
-       document.getElementById("demo").innerHTML = "Clear Text"
+       document.getElementById("left").innerHTML = "Clear Text"
+       document.getElementById("right").innerHTML = "Clear Text"
        $scope.sa = undefined;
        $scope.ba = undefined;
    }
